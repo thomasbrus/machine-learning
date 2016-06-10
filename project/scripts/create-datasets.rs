0b663eff42f57459875f5b95883b54750ce1cb58
Thomas Brus <thomas.brus@me.com>
2016-06-10 12:56:01 +0200

    Fixed script for windows & added missing gitkeep files

--- project/scripts/create-datasets.rs
+++ project/scripts/create-datasets.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufWriter;
+use std::path::PathBuf;
 use std::io::prelude::*;
 use std::io;
 
@@ -9,7 +10,7 @@ fn write_line(writer: &mut BufWriter<File>, line: &str) -> usize {
     writer.write(format!("{}\n", line).as_bytes()).expect("Failed to write line")
 }
 
-fn create_dataset(input_name: &str, output_name: &str, nth: i32) -> Result<(), io::Error> {
+fn create_dataset(input_name: &str, output_name: PathBuf, nth: i32) -> Result<(), io::Error> {
     let mut row_number = 0;
 
     let input_file = try!(File::open(input_name));
@@ -30,6 +31,15 @@ fn create_dataset(input_name: &str, output_name: &str, nth: i32) -> Result<(), i
     Ok(())
 }
 
+fn build_output_path(dir: &str, filename: &str) -> PathBuf {
+    let mut path = PathBuf::new();
+    path.push("datasets");
+    path.push(dir);
+    path.push(filename);
+    path.set_extension("csv");
+    path
+}
+
 fn main() {
     // For example ./scripts/create-datasets datasets/train.csv datasets/test.csv
     let filename1 = &env::args().nth(1).expect("Missing argument <train.csv>");
@@ -41,12 +51,12 @@ fn main() {
     let prime3 = 1;
 
     // Take each nth line and output to file
-    create_dataset(filename1, "datasets/a/train.csv", prime1).unwrap();
-    create_dataset(filename1, "datasets/b/train.csv", prime2).unwrap();
-    create_dataset(filename1, "datasets/c/train.csv", prime3).unwrap();
+    create_dataset(filename1, build_output_path("a", "train.csv"), prime1).unwrap();
+    create_dataset(filename1, build_output_path("b", "train.csv"), prime2).unwrap();
+    create_dataset(filename1, build_output_path("c", "train.csv"), prime3).unwrap();
 
     // Idem dito for test sets
-    create_dataset(filename2, "datasets/a/test.csv", prime1).unwrap();
-    create_dataset(filename2, "datasets/b/test.csv", prime2).unwrap();
-    create_dataset(filename2, "datasets/c/test.csv", prime3).unwrap();
+    create_dataset(filename2, build_output_path("a", "test.csv"), prime1).unwrap();
+    create_dataset(filename2, build_output_path("b", "test.csv"), prime2).unwrap();
+    create_dataset(filename2, build_output_path("c", "test.csv"), prime3).unwrap();
 }
